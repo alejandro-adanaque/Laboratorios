44d3d5729a2d4e4a089b2d9172f8f77c568f37b3
alejandro-adanaque <42155955+alejandro-adanaque@users.noreply.github.com>
2018-08-13 11:24:43 -0500

    Laboratorio semana 10

---
+++ Laborarotios/CostoDeLlamadas/08CostoDeLlamadas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08CostoDeLlamadas
+{
+    class Program
+    {
+        static void titulo()
+        {
+            Console.Write("Bienvenido: a \"SENATI\"\n");
+            Console.WriteLine("========================================");
+        }
+
+        static void Main(string[] args)
+        {
+            //Calcula el costo de una llamada
+
+            const double PRECIO_X_MINUTO = 0.25;
+            titulo();
+
+            Console.Write("Ingrese cantidad de minutos de la llamada: ");
+            int cantMinutos = int.Parse(Console.ReadLine());
+
+            double precioAPagar = PRECIO_X_MINUTO * cantMinutos;
+
+            Console.WriteLine($"Usted pagará por {cantMinutos} minutos un total de:  {precioAPagar.ToString("c2")}" );
+
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laborarotios/EntrevistaDeTrabajo/08EntrevistaDeTrabajo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08EntrevistaDeTrabajo
+{
+    class Program
+    {
+        static int calculaEdad(int anioNacimiento)
+        {
+            DateTime fechaActual = DateTime.Now; //Obtiene la fecha de la computadora
+            int anioActual = fechaActual.Year; //Obtiene el año actual
+            int mesActual = fechaActual.Month; //Obtiene el mes
+            int diaActual = fechaActual.Day;   //Obtiene el dia
+
+            int edadActual = anioActual - anioNacimiento; //Obtiene edad
+            return edadActual;
+        }
+
+        static void titulo()
+        {
+            Console.WriteLine("Bienvenido a \"SENATI\"\n");
+            Console.WriteLine("================================================");
+        }
+
+        static void Main(string[] args)
+        {
+            //Programa que calcula las edades de personas en una entrevista
+
+            titulo();
+
+            Console.Write("Ingrese su Nombre: ");
+            String nombre = Console.ReadLine();
+
+            Console.Write("Ingrese año de naciemiento: ");
+            int anioNacimiento = int.Parse(Console.ReadLine());
+
+            int edad = calculaEdad(anioNacimiento);
+
+            Console.WriteLine($"Bienvenido {nombre} usted tiene {edad} para la entrevista");
+
+
+            Console.ReadKey();
+        }
+    }
+}
